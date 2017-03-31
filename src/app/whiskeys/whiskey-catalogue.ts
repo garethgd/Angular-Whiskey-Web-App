161c2a2a30065f260e136d9e72a16118fbc4080f
garethgd <garethgdunne@gmail.com>
2017-03-31 18:03:44 +0100

    text change

--- src/app/whiskeys/whiskey-catalogue.ts
+++ src/app/whiskeys/whiskey-catalogue.ts
@@ -2,7 +2,7 @@ import { Whiskey } from './whiskey';
 
 
 export const WHISKEY_CAT = [
-    new Whiskey("Jameson", 22 , 'Florida', 'Jacksonville', 'An Oak that grew on the bank of a river was uprooted by a severe gale of wind, and thrown across the stream.', '/assets/clontarf-whiskey.jpg', 0),
+    new Whiskey("Clontarf", 22 , 'Florida', 'Jacksonville', 'An Oak that grew on the bank of a river was uprooted by a severe gale of wind, and thrown across the stream.', '/assets/clontarf-whiskey.jpg', 0),
      new Whiskey(" Wood Emerald", 26 , 'Glasgow', 'Jacksonville', 'An Oak that grew on the bank of a river was uprooted by a severe gale of wind, and thrown across the stream.', '/assets/kilbeggan_whiskey.jpg', 3),
-      new Whiskey("Paddys", 42 , 'Ireland', 'Galway', 'his blend has always been a mix of both pot still whiskeys, grain and also some single malt.Paddy is best described as unpretentious and friendly,', '/assets/clontarf-whiskey.jpg', 1),
+      new Whiskey("Knappogue Whiskey", 42 , 'Ireland', 'Galway', 'his blend has always been a mix of both pot still whiskeys, grain and also some single malt.Paddy is best described as unpretentious and friendly,', '/assets/Knappogue_Whiskey.jpg', 1),
 ]
