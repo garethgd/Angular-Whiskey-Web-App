52189939fa3bd838b455beedd55eed08ce997aaa
garethgd <garethgdunne@gmail.com>
2017-04-01 12:27:03 +0100

    foote

--- src/app/shared/whiskey/whiskey-catalogue.ts
+++ src/app/shared/whiskey/whiskey-catalogue.ts
@@ -2,7 +2,7 @@ import { Whiskey } from './Whiskey';
 
 
 export const WHISKEY_CAT = [
-    new Whiskey("Jameson", 22, 'Florida', 'Jacksonville', 'An Oak that grew on the bank of a river was uprooted by a severe gale of wind, and thrown across the stream.', '/assets/clontarf-whiskey.jpg'),
+    new Whiskey("Clontarf", 22, 'Florida', 'Jacksonville', 'An Oak that grew on the bank of a river was uprooted by a severe gale of wind, and thrown across the stream.', '/assets/clontarf-whiskey.jpg'),
     new Whiskey(" Wood Emerald", 26, 'Glasgow', 'Jacksonville', 'An Oak that grew on the bank of a river was uprooted by a severe gale of wind, and thrown across the stream.', '/assets//kilbeggan_whiskey.jpg'),
     new Whiskey("Knappogue", 42, 'Ireland', 'Galway', 'his blend has always been a mix of both pot still whiskeys, grain and also some single malt.Paddy is best described as unpretentious and friendly,', '/assets/Knappogue_Whiskey.jpg'),
 ]
